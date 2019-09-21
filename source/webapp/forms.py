130711ae930117f46bef3b1c090a79587251d92b
464716 <diesel.kg@mail.ru>
2019-09-21 13:35:19 +0600

    создал главную страницу

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from django.forms import widgets
+from webapp.models import status_choices
 
 
-class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=200, required=True, label='Заголовок')
+class BookForm(forms.Form):
+    author = forms.CharField(max_length=40, required=True, label='Автор', empty_value="Unknown")
+    email = forms.EmailField(max_length=100, required=True, label='E-mail')
     text = forms.CharField(max_length=3000, required=True, label='Текст статьи', widget=widgets.Textarea)
-    author = forms.CharField(max_length=40, required=False, label='Автор', empty_value="Unknown")
+    # created_at = forms.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    # updated_at = forms.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    # status = forms.ChoiceField(required=False, label='Status', initial="active", choices=status_choices)
